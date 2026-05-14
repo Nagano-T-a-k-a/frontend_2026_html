1ce27764f62e4961ce54af6905cdde636e430242
nagano.takahiro <nagano.takahiro@gxp.co.jp>
2026-05-14 15:36:09 +0900

    JS 8-3 done

--- javascript/07_conditionals/03_challenge/script.js
+++ javascript/07_conditionals/03_challenge/script.js
@@ -8,4 +8,38 @@ function showDaysInMonth() {
   //   - 2月 → うるう年なら29日、それ以外は28日
   // ヒント: 三項演算子でうるう年かどうかを表示する
   // ヒント: #days-output の textContent に結果を表示する
+  const month = Number(document.getElementById('month-input').value);
+  const year = Number(document.getElementById('year-input').value);
+  const output = document.getElementById('days-output');
+  output.textContent = '';
+
+  const isLeap = (year) => year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  let days = 0;
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      days = 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days = 30;
+      break;
+    case 2:
+      days = isLeap(year) ? 29 : 28;
+      break;
+  }
+
+  let p = document.createElement('p');
+  p.textContent = days + '日';
+  output.appendChild(p);
+  p = document.createElement('p');
+  p.textContent = isLeap(year);
+  output.appendChild(p);
 }
